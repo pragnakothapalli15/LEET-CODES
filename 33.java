f7dfdb0ad35addded8ba67c9a5c5c3dd2941e097
pragnakothapalli15 <pragnakothapalli15@gmail.com>
2026-07-22 20:27:35 +0530

    Create 33.java

---
+++ 33.java
@@ -0,0 +1,46 @@
+class Solution {
+
+    public int[] searchRange(int[] nums, int target) {
+        return new int[] { firstPosition(nums, target), lastPosition(nums, target) };
+    }
+
+    private int firstPosition(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int ans = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                right = mid - 1;   // Search left
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+    private int lastPosition(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int ans = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                ans = mid;
+                left = mid + 1;    // Search right
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+}
