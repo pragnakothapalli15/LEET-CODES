95053c627bd848c387b4916ff01b7ca8d1bec97f
pragnakothapalli15 <pragnakothapalli15@gmail.com>
2026-07-25 20:30:19 +0530

    Create 51.java

---
+++ 51.java
@@ -0,0 +1,44 @@
+class Solution {
+    int count = 0;
+
+    public int totalNQueens(int n) {
+        boolean[] cols = new boolean[n];
+        boolean[] diag = new boolean[2 * n - 1];
+        boolean[] antiDiag = new boolean[2 * n - 1];
+
+        backtrack(0, n, cols, diag, antiDiag);
+
+        return count;
+    }
+
+    private void backtrack(int row, int n,
+                           boolean[] cols,
+                           boolean[] diag,
+                           boolean[] antiDiag) {
+
+        if (row == n) {
+            count++;
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+
+            int d = row - col + n - 1;
+            int ad = row + col;
+
+            if (cols[col] || diag[d] || antiDiag[ad]) {
+                continue;
+            }
+
+            cols[col] = true;
+            diag[d] = true;
+            antiDiag[ad] = true;
+
+            backtrack(row + 1, n, cols, diag, antiDiag);
+
+            cols[col] = false;
+            diag[d] = false;
+            antiDiag[ad] = false;
+        }
+    }
+}
